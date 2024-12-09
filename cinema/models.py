5c0008b31435c5259dd4f39929c7a08279573c10
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-12-09 02:54:30 +0200

    Create API views for movie endpoints

--- cinema/models.py
+++ cinema/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
+    description = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
     duration = models.PositiveIntegerField()
 
     def __str__(self) -> str:
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+@api_view(["GET", "POST"])
+def movie_list(request: HttpRequest) -> Response:
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request: HttpRequest, pk: int) -> Response:
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
